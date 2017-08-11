e463afa476e1d2a804a435f4b62804d74e3e2f65
御坂美琴auautyan <autyan@outlook.com>
2017-08-11 18:53:04 +0800

    添加部分逻辑

--- ProtocolCommunicationService/Core/IOTDevice.cs
+++ ProtocolCommunicationService/Core/IOTDevice.cs
@@ -2,6 +2,7 @@
 // ReSharper disable InconsistentNaming
 
 using System;
+using ProtocolCommunicationService.NetWorkCore;
 using SHWDTech.IOT.Storage.Communication;
 
 namespace ProtocolCommunicationService.Core
@@ -10,14 +11,83 @@ namespace ProtocolCommunicationService.Core
     {
         private readonly Device _device;
 
+        private DeviceClient _deviceClient;
+
         public string Name => _device.DeviceName;
 
         public string NodeIdHexString => _device.NodeIdHexString;
-        
+
+        public DateTime ConnectedTime { get; private set; }
+
+        public DateTime DisconnectedTime { get; private set; }
+
+        public DateTime HeartBeatTime { get; private set; }
+
+        public DateTime ReceiveDataTime { get; private set; }
+
+        public DateTime SendDataTime { get; private set; }
+
+        public DateTime ProcessProtocolTime { get; private set; }
 
         public IOTDevice(Device dev)
         {
             _device = dev;
+        }
+
+        public void SetupClient(DeviceClient client)
+        {
+            _deviceClient?.Dispose();
+            _deviceClient = client;
+            _deviceClient.OnDataReceived += DataReceived;
+            _deviceClient.OnDataSend += DataSend;
+            _deviceClient.OnDisconnected += Disconnected;
+        }
+
+        public void UpdateConnectTime()
+        {
+            ConnectedTime = DateTime.Now;
+        }
+
+        public void UpdateDisconnectTime()
+        {
+            DisconnectedTime = DateTime.Now;
+        }
+
+        public void UpdateHeartBeatTime()
+        {
+            HeartBeatTime = DateTime.Now;
+        }
+
+        public void UpdateReceiveDataTime()
+        {
+            ReceiveDataTime = DateTime.Now;
+        }
+
+        public void UpdateSendDataTime()
+        {
+            SendDataTime = DateTime.Now;
+        }
+
+        public void UpdateProcessProtocolTime()
+        {
+            ProcessProtocolTime = DateTime.Now;
+        }
+
+        private void DataReceived(ClientDataReceivedEventArgs args)
+        {
+            UpdateReceiveDataTime();
+        }
+
+        private void DataSend(ClientSendDataEventArgs args)
+        {
+            UpdateSendDataTime();
+        }
+
+        private void Disconnected(ClientDisconnectedEventArgs args)
+        {
+            UpdateDisconnectTime();
+            _deviceClient?.Dispose();
+            _deviceClient = null;
         }
     }
 }
